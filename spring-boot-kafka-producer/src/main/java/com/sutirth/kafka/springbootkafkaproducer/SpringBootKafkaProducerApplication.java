969194ebad30cb4a7f284b63f46c0080c42e0651
ricksr <sutirtharej@lidolearning.com>
2020-07-05 18:41:01 +0530

    kafka-producer-added

---
+++ spring-boot-kafka-producer/src/main/java/com/sutirth/kafka/springbootkafkaproducer/SpringBootKafkaProducerApplication.java
@@ -0,0 +1,14 @@
+package com.sutirth.kafka.springbootkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaProducerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ spring-boot-kafka-producer/src/main/java/com/sutirth/kafka/springbootkafkaproducer/resource/UserResource.java
@@ -0,0 +1,26 @@
+package com.sutirth.kafka.springbootkafkaproducer.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private  static final String TOPIC = "kafka-exampl";
+
+    @GetMapping("/publish/{message}")
+    public String post(@PathVariable("message") final String message) {
+
+        kafkaTemplate.send(TOPIC, message);
+
+        return "published successfully";
+    }
+}
